7d6767083cd035d692caa6cb303d7090e00893a5
josepardoserpis <josana@alu.iesserpis.org>
2020-01-29 10:39:03 +0100

    Primer ejercicio de fragmentos, sin terminar

---
+++ FragmentoBasico/app/src/main/java/com/example/fragmentobasico/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.fragmentobasico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    View miFragmento;
+    CheckBox apareceFrag;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        miFragmento = (View)findViewById(R.id.ejemplo);
+        apareceFrag = (CheckBox)findViewById(R.id.checkFrag);
+        apareceFrag.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (apareceFrag.isChecked()){miFragmento.setVisibility(View.VISIBLE);
+                }else {
+                    miFragmento.setVisibility(View.INVISIBLE);
+                }
+
+            }
+        });
+    }
+}
